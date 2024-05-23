5c857a472f1032917bff7ad77b7b274f9b2df73d
Ryan <ryandaltrey@hotmail.com>
2024-05-23 10:42:05 +0200

    ADD dans Models/Livres son class

---
+++ Models/Livres.js
@@ -0,0 +1,42 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require ('../Config/Sequelize');
+
+class Livres extends Model{
+
+}
+
+Livres.init({
+
+    li_id : {
+        type : DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    li_titre: {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    li_genre: {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    li_datePublication: {
+        type : DataTypes.DATE,
+        allowNull: false
+    },
+    li_editeur: {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    li_description: {
+        type : DataTypes.STRING,
+        allowNull: true
+    }
+}, {
+    sequelize,
+    modelName: 'Livres',
+    tableName :'livres',
+    timestamps: false,
+})
+
+module.exports = Livres;
