49fa073a9d39e44bafa71bfafbdf060a11d5756e
Ryan <ryandaltrey@hotmail.com>
2024-05-23 10:25:35 +0200

    ADD dans Models -Auteurs son class

---
+++ Models/Auteurs.js
@@ -0,0 +1,37 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require ('../Config/Sequelize');
+
+class Auteurs extends Model{
+
+}
+
+Auteurs.init({
+    au_id : {
+        type : DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    au_nom : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    au_prenom : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    au_ddn : {
+        type : DataTypes.DATE,
+        allowNull : false
+    },
+    au_nationalite :{
+        type : DataTypes.STRING,
+        allowNull : false
+    }
+}, {
+    sequelize,
+    modelName : 'Auteurs',
+    tableName : 'auteurs',
+    timestamps: false,
+ 
+})
+module.exports = Auteurs;
