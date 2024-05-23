c03ae4998670f0315c88ff5658aaa83ae023de34
Ryan <ryandaltrey@hotmail.com>
2024-05-23 11:32:01 +0200

    ADD dans models-emprunts son class

---
+++ Models/Emprunts.js
@@ -0,0 +1,42 @@
+const {Model, DataTypes} = require ('sequelize');
+const sequelize = require ('../Config/Sequelize');
+const livres = require ('./Livres');
+const emprunteurs = require ('./Emprunteurs');
+
+class Emprunts extends Model{
+
+}
+
+Emprunts.init({
+    emp_id: {
+        type : DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    emp_dateDebut : {
+        type : DataTypes.DATE,
+        allowNull : false
+    },
+    emp_dateFin : {
+        type : DataTypes.DATE,
+        allowNull : false
+    },
+    emp_quantite : {
+        type : DataTypes.INTEGER,
+        allowNull : true
+    },
+    li_id : {
+        type : DataTypes.INTEGER,
+        allowNull: false
+    },
+    empr_id : {
+        type : DataTypes.INTEGER,
+        allowNull: false
+    }
+},{
+    sequelize,
+    modelName: 'Emprunts',
+    tableName : 'emprunts',
+    timestamps: false
+})
+module.exports = Emprunts;
