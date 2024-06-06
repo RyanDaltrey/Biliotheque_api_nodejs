f1d3afaf6b7257791922a43f53e65a807075278c
Ryan <ryandaltrey@hotmail.com>
2024-06-06 10:20:16 +0200

    ADD cors pour donné acces au localhost par rapport au Token

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const auteursRoutes = require ('./Routes/AuteursRoutes');
 const livresRoutes = require ('./Routes/LivresRoutes');
@@ -8,10 +9,15 @@ const authenticateRoutes = require('./Routes/AuthenticateRoutes');
 const AuthenticateController = require('./Controllers/AuthenticateControllers');
 
 app.use(express.json());
-app.use('/auteurs',AuthenticateController.authenticateToken, auteursRoutes);
-app.use('/livres', AuthenticateController.authenticateToken, livresRoutes);
-app.use('/emprunts', AuthenticateController.authenticateToken,  empruntsRoutes);
-app.use('/emprunteurs', AuthenticateController.authenticateToken,  emprunteursRoutes );
-app.use('/auth', authenticateRoutes)
+app.use(cors());
+// app.use('/auteurs',AuthenticateController.authenticateToken, auteursRoutes);
+// app.use('/livres', AuthenticateController.authenticateToken, livresRoutes);
+app.use('/auteurs', auteursRoutes);
+app.use('/livres', livresRoutes);
+// app.use('/emprunts', AuthenticateController.authenticateToken,  empruntsRoutes);
+// app.use('/emprunteurs', AuthenticateController.authenticateToken,  emprunteursRoutes );
+app.use('/emprunteurs', emprunteursRoutes);
+app.use('/emprunts', empruntsRoutes);
+app.use('/auth', authenticateRoutes);
 
 module.exports = app;
